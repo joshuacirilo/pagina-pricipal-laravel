6181e877664599fd0b4f10d9d0bdc163a92c4203
AquinoB2090 <brandonjose2090@gmail.com>
2026-08-11 22:51:59 -0600

    Ollama

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         ],
     ],
 
+    'ollama' => [
+        'url' => env('OLLAMA_URL', 'http://localhost:11434'),
+        'embedding_model' => env('OLLAMA_EMBEDDING_MODEL', 'nomic-embed-text'),
+        'chat_model' => env('OLLAMA_CHAT_MODEL', 'qwen2.5:3b'),
+    ],
+
 ];
